2ffbe2a9aac644b1b7e8c5f1260ee8e18f39aaf5
grandmasponge <tobyr2006@gmail.com>
2023-12-12 00:00:56 +0000

    final commit

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
-use std::mem::size_of;
+use std::mem::{size_of, MaybeUninit};
+use std::str;
 
-use windows::core::Error;
-use windows::Win32::Foundation::{self, CloseHandle, GetLastError, BOOL};
-use windows::Win32::System::ProcessStatus::{self, EnumProcesses};
-use windows::Win32::System::Threading::{OpenProcess, PROCESS_QUERY_INFORMATION};
+use windows::Win32::Foundation::{CloseHandle, GetLastError, BOOL, FALSE, HMODULE};
+use windows::Win32::System::ProcessStatus::{
+    EnumProcessModules, EnumProcesses, GetModuleBaseNameA,
+};
+use windows::Win32::System::Threading::{
+    OpenProcess, PROCESS_ALL_ACCESS, PROCESS_QUERY_INFORMATION, PROCESS_QUERY_LIMITED_INFORMATION,
+    PROCESS_VM_OPERATION, PROCESS_VM_READ, PROCESS_VM_WRITE,
+};
 
 fn main() -> Result<(), ()> {
-    let mut processes: [u32; 1024] = [0; 1024];
-    let cb: u32 = size_of::<u32>() as u32;
+    let mut processes = Vec::<u32>::with_capacity(1024);
+    let cb: u32 = 1024;
     let mut lpcbneeded: u32 = 0;
 
     unsafe {
@@ -27,35 +32,74 @@ fn main() -> Result<(), ()> {
         }
     }
 
+    unsafe {
+        processes.set_len(lpcbneeded as usize);
+    }
+
     let num_of_processes = lpcbneeded / 4;
 
-    for i in 0..num_of_processes as usize {
+    for i in 100..num_of_processes as usize {
         let proccess_id = processes[i];
 
-        let process_handle = unsafe { OpenProcess(PROCESS_QUERY_INFORMATION, BOOL(0), 18260) };
+        if proccess_id == 0 {
+            continue;
+        }
+
+        let process_handle = unsafe {
+            OpenProcess(
+                PROCESS_ALL_ACCESS | PROCESS_QUERY_LIMITED_INFORMATION | PROCESS_VM_READ,
+                FALSE,
+                proccess_id,
+            )
+        };
         let handle = match process_handle {
             Ok(handle) => handle,
             Err(err) => {
-                println!("error {:?}", err);
+                println!("{:?}", err);
                 return Err(());
             }
         };
 
+        let mut hmodule = MaybeUninit::<HMODULE>::uninit();
+        let mut size = 0;
 
+        let res = unsafe {
+            EnumProcessModules(
+                handle,
+                hmodule.as_mut_ptr(),
+                size_of::<HMODULE>() as u32,
+                &mut size,
+            )
+        };
+
+        match res {
+            Ok(()) => {}
+            Err(err) => {
+                println!("{err}");
+                return Err(());
+            }
+        }
+
+        let mut process_name: [u8; 1024] = [0; 1024];
+
+        let hmodule = unsafe { hmodule.assume_init() };
+
+        let _processinfo = unsafe { GetModuleBaseNameA(handle, hmodule, &mut process_name) };
+
+        let process_name = str::from_utf8(&process_name).unwrap();
+
+        println!("Process: {process_name} {proccess_id}");
 
         unsafe {
             match CloseHandle(handle) {
-                Ok(()) => {
-                    println!("closing handle");
-                }
+                Ok(()) => {}
                 Err(err) => {
                     println!("failed to close handle");
-                    println!("Error : {:?}", err);
+                    println!("{:?}", err);
                     return Err(());
                 }
             }
         }
     }
-
     Ok(())
 }
